8c84e7fbb9dbbbb5923bd98bcd8aa0bd8f89c73f
FORHADUL ISLAM <mdforhadul44@gmail.com>
2022-07-17 23:00:22 +0600

    add file

--- schoole_managment_system/controller/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Admin
-
-# Register your models here.
-admin.site.register(Admin)
--- schoole_managment_system/teacher/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Teacher, TeacherPayment
-
-# Register your models here.
-admin.site.register(Teacher)
-admin.site.register(TeacherPayment)
